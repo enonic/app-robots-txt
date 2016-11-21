caeb17d38ad3d199696ec074669399ff4f4f9fef
Sepideh <ska@enonic.com>
2016-11-21 15:53:42 +0100

    robots.txt

---
+++ src/main/resources/services/robotstxt/robotstxt.js
@@ -0,0 +1,16 @@
+var libs = {
+    portal: require('/lib/xp/portal')
+};
+
+
+function handleGet(req) {
+
+    var siteConfig = libs.portal.getSiteConfig();
+    return {
+        contentType: 'text/plain',
+        body: siteConfig.robotstxt,
+    };
+}
+exports.get = handleGet;
+
+
